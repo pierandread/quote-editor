855386629a70db7fe315c1742b7ae2e60eef093f
Pier Andrea <pierandreadelise@gmail.com>
2025-04-08 14:00:29 +0200

    turbo-streams-security

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  # devise parent class
+  include Warden::Test::Helpers
+
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
 end
